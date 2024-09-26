000689e70d6947962cc774fd818a802351e930d8
anil <amuseranil@gmail.com>
2024-09-26 17:12:57 -0500

    git source

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,6 @@
 module "vpc" {
-  source                = "../terraform-aws-modules/vpc"
+  #source                = "../terraform-aws-modules/vpc"
+  source                = "git::https://github.com/Anilgitg/terraform-aws-modules.git?ref=main" 
   vpc_cidr_block        = var.vpc_cidr_block
   project_name          = var.project_name
   environment           = var.environment
